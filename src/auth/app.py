52a14c5765ecaee4712d0d5cccc1c63017ad52fa
Comicly69 <snowyoumc@gmail.com>
2023-05-20 20:31:15 -0400

    mod auth

--- src/auth/app.py
+++ src/auth/app.py
@@ -1,4 +1,19 @@
 import chip
-import time
+import datetime
 
-chip.get()
+# App
+chip_data = chip.get()
+chip_time = chip_data["time"]
+
+current_time = datetime.datetime.now()
+time_diff = current_time - chip_time
+
+print(f"Version: {chip_data['version']}")
+print(f"Id: {chip_data['privateIdentifier']}")
+print(f"Public Id: {chip_data['publicIdentifier']}")
+print(f"Transactions: {chip_data['transactions']}")
+print(f"Time: {chip_time}")
+print(f"Time difference: {time_diff}")
+
+if time_diff.total_seconds() > 3:
+    print("WARNING: TIME DIFFERENCE OVER RECOMMENDED")
--- src/auth/chip.py
+++ src/auth/chip.py
@@ -1,15 +1,33 @@
 import random
 import string
 import datetime
+import hashlib
 
+# Chip
 version = "0.0.1"
-identifier = ''.join(random.choices(string.ascii_letters + string.digits, k=12))
+privateIdentifier = ''.join(random.choices(string.ascii_letters + string.digits, k=12))
+publicIdentifier = hashlib.sha256(privateIdentifier.encode()).hexdigest()
 transactions = 0
-time = datetime.datetime.now
+time = datetime.datetime.now()
+
+#cardData
+cdata = [
+    {
+        "cnum" : "1"
+    }
+]
 
 
 def get():
-    print(f"Version: {version}")
-    print(f"Id: {identifier}")
-    print(f"Transactions: {transactions}")
-    print(f"Time: {time}")
+    return {
+        "version": version,
+        "privateIdentifier": privateIdentifier,
+        "publicIdentifier": publicIdentifier,
+        "transactions": transactions,
+        "time": time
+    }
+
+class transaction():
+
+    def send():
+        pass
